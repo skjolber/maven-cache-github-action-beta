9df7927e606f628840ee901326ac450a2726427a
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2021-02-07 21:19:12 +0100

    Test build

--- src/restore.ts
+++ src/restore.ts
@@ -65,7 +65,7 @@ async function runGitCommand(parameters : Array<string>) : Promise<GitOutput> {
   let errorOut = '';
 
   await exec.exec('git', parameters, {
-      silent: true,
+      silent: false,
       failOnStdErr: false,
       ignoreReturnCode: false,
       listeners: {
