c9a65df31cf6b1c554d295306ee9f20ea325f8e5
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2024-09-16 23:03:54 +0200

    Fix various linting

--- src/save.ts
+++ src/save.ts
@@ -1,60 +1,72 @@
 import * as cache from "@actions/cache";
 import * as core from "@actions/core";
+
+import { CachePaths, Inputs, State } from "./constants";
+import * as utils from "./utils/actionUtils";
 import * as maven from "./utils/maven";
 
-import { Events, Inputs, State, CachePaths } from "./constants";
-import * as utils from "./utils/actionUtils";
-
 async function run(): Promise<void> {
+    // so job failed
+    // however was the cache already saved
+    const step = core.getState(State.Step);
 
-  // so job failed
-  // however was the cache already saved
-  const step = core.getState(State.Step);
+    if (step === "restore") {
+        const hash = core.getState(State.FailureHash);
 
-  if(step === "restore") {
-    const hash = core.getState(State.FailureHash);
+        if (hash.length > 0) {
+            console.log("Save cache for failed build..");
 
-    if(hash.length > 0) {
-        console.log("Save cache for failed build..");
+            // nuke resolution attempts, so that resolution is always reattempted on next build
 
-        // nuke resolution attempts, so that resolution is always reattempted on next build
-
-        const enableCrossOsArchive = utils.getInputAsBool(
-            Inputs.EnableCrossOsArchive
-        );
-
-        await maven.removeResolutionAttempts(CachePaths);
-
-        try {
-            await cache.saveCache(CachePaths, hash, {
-                uploadChunkSize: utils.getInputAsInt(Inputs.UploadChunkSize)
-            }, enableCrossOsArchive);
-            console.log("Cache saved for failed build. Another cache will be saved once the build is successful.")
-
-            const cacheId = await cache.saveCache(
-                CachePaths, hash,
-                { uploadChunkSize: utils.getInputAsInt(Inputs.UploadChunkSize) },
-                enableCrossOsArchive
+            const enableCrossOsArchive = utils.getInputAsBool(
+                Inputs.EnableCrossOsArchive
             );
-    
-            if (cacheId != -1) {
-                core.info(`Cache saved with key: ${hash}`);
-            }
-        } catch (err: unknown) {
-            const error = err as Error
-            if (error.name === cache.ValidationError.name) {
-                throw error;
-            } else if (error.name === cache.ReserveCacheError.name) {
-                core.info(error.message);
-            } else {
-                utils.logWarning(error.message);
-            }
-        }
-      } else {
-          console.log("Do not save cache for failed build");
-      }
-  }
 
+            await maven.removeResolutionAttempts(CachePaths);
+
+            try {
+                await cache.saveCache(
+                    CachePaths,
+                    hash,
+                    {
+                        uploadChunkSize: utils.getInputAsInt(
+                            Inputs.UploadChunkSize
+                        )
+                    },
+                    enableCrossOsArchive
+                );
+                console.log(
+                    "Cache saved for failed build. Another cache will be saved once the build is successful."
+                );
+
+                const cacheId = await cache.saveCache(
+                    CachePaths,
+                    hash,
+                    {
+                        uploadChunkSize: utils.getInputAsInt(
+                            Inputs.UploadChunkSize
+                        )
+                    },
+                    enableCrossOsArchive
+                );
+
+                if (cacheId != -1) {
+                    core.info(`Cache saved with key: ${hash}`);
+                }
+            } catch (err: unknown) {
+                const error = err as Error;
+                if (error.name === cache.ValidationError.name) {
+                    throw error;
+                } else if (error.name === cache.ReserveCacheError.name) {
+                    core.info(error.message);
+                } else {
+                    utils.logWarning(error.message);
+                }
+            }
+        } else {
+            console.log("Do not save cache for failed build");
+        }
+    }
 }
 
 run();
--- src/utils/requestUtils.ts
+++ src/utils/requestUtils.ts
@@ -1,105 +1,106 @@
-import * as core from '@actions/core'
-import {HttpCodes, HttpClientError, HttpClientResponse} from '@actions/http-client'
-import {DefaultRetryDelay, DefaultRetryAttempts} from './constants'
+import * as core from "@actions/core";
+import { HttpClientResponse, HttpCodes } from "@actions/http-client";
+
+import { DefaultRetryAttempts, DefaultRetryDelay } from "./constants";
 
 export function isSuccessStatusCode(statusCode?: number): boolean {
-  if (!statusCode) {
-    return false
-  }
-  return statusCode >= 200 && statusCode < 300
+    if (!statusCode) {
+        return false;
+    }
+    return statusCode >= 200 && statusCode < 300;
 }
 
 export function isServerErrorStatusCode(statusCode?: number): boolean {
-  if (!statusCode) {
-    return true
-  }
-  return statusCode >= 500
+    if (!statusCode) {
+        return true;
+    }
+    return statusCode >= 500;
 }
 
 export function isRetryableStatusCode(statusCode?: number): boolean {
-  if (!statusCode) {
-    return false
-  }
-  const retryableStatusCodes = [
-    HttpCodes.BadGateway,
-    HttpCodes.ServiceUnavailable,
-    HttpCodes.GatewayTimeout
-  ]
-  return retryableStatusCodes.includes(statusCode)
+    if (!statusCode) {
+        return false;
+    }
+    const retryableStatusCodes = [
+        HttpCodes.BadGateway,
+        HttpCodes.ServiceUnavailable,
+        HttpCodes.GatewayTimeout
+    ];
+    return retryableStatusCodes.includes(statusCode);
 }
 
 async function sleep(milliseconds: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, milliseconds))
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
 export async function retry<T>(
-  name: string,
-  method: () => Promise<T>,
-  getStatusCode: (arg0: T) => number | undefined,
-  maxAttempts = DefaultRetryAttempts,
-  delay = DefaultRetryDelay,
-  onError: ((arg0: Error) => T | undefined) | undefined = undefined
+    name: string,
+    method: () => Promise<T>,
+    getStatusCode: (arg0: T) => number | undefined,
+    maxAttempts = DefaultRetryAttempts,
+    delay = DefaultRetryDelay,
+    onError: ((arg0: Error) => T | undefined) | undefined = undefined
 ): Promise<T> {
-  let errorMessage = ''
-  let attempt = 1
+    let errorMessage = "";
+    let attempt = 1;
 
-  while (attempt <= maxAttempts) {
-    let response: T | undefined = undefined
-    let statusCode: number | undefined = undefined
-    let isRetryable = false
+    while (attempt <= maxAttempts) {
+        let response: T | undefined = undefined;
+        let statusCode: number | undefined = undefined;
+        let isRetryable = false;
 
-    try {
-      response = await method()
-    } catch (err: unknown) {
-      const error = err as Error
-      if (onError) {
-        response = onError(error)
-      }
+        try {
+            response = await method();
+        } catch (err: unknown) {
+            const error = err as Error;
+            if (onError) {
+                response = onError(error);
+            }
 
-      isRetryable = true
-      errorMessage = error.message
+            isRetryable = true;
+            errorMessage = error.message;
+        }
+
+        if (response) {
+            statusCode = getStatusCode(response);
+
+            if (!isServerErrorStatusCode(statusCode)) {
+                return response;
+            }
+        }
+
+        if (statusCode) {
+            isRetryable = isRetryableStatusCode(statusCode);
+            errorMessage = `Cache service responded with ${statusCode}`;
+        }
+
+        core.debug(
+            `${name} - Attempt ${attempt} of ${maxAttempts} failed with error: ${errorMessage}`
+        );
+
+        if (!isRetryable) {
+            core.debug(`${name} - Error is not retryable`);
+            break;
+        }
+
+        await sleep(delay);
+        attempt++;
     }
 
-    if (response) {
-      statusCode = getStatusCode(response)
-
-      if (!isServerErrorStatusCode(statusCode)) {
-        return response
-      }
-    }
-
-    if (statusCode) {
-      isRetryable = isRetryableStatusCode(statusCode)
-      errorMessage = `Cache service responded with ${statusCode}`
-    }
-
-    core.debug(
-      `${name} - Attempt ${attempt} of ${maxAttempts} failed with error: ${errorMessage}`
-    )
-
-    if (!isRetryable) {
-      core.debug(`${name} - Error is not retryable`)
-      break
-    }
-
-    await sleep(delay)
-    attempt++
-  }
-
-  throw Error(`${name} failed: ${errorMessage}`)
+    throw Error(`${name} failed: ${errorMessage}`);
 }
 
-export async function retryHttpClientResponse<T>(
-  name: string,
-  method: () => Promise<HttpClientResponse>,
-  maxAttempts = DefaultRetryAttempts,
-  delay = DefaultRetryDelay
+export async function retryHttpClientResponse(
+    name: string,
+    method: () => Promise<HttpClientResponse>,
+    maxAttempts = DefaultRetryAttempts,
+    delay = DefaultRetryDelay
 ): Promise<HttpClientResponse> {
-  return await retry(
-    name,
-    method,
-    (response: HttpClientResponse) => response.message.statusCode,
-    maxAttempts,
-    delay
-  )
+    return await retry(
+        name,
+        method,
+        (response: HttpClientResponse) => response.message.statusCode,
+        maxAttempts,
+        delay
+    );
 }
